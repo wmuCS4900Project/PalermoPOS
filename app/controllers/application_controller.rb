bb7711b9a9d4b2feaa42929c8a9ef566472344ea
Joseph Owens <jtrumanowens@gmail.com>
2017-02-24 00:12:57 +0000

    redirect to force being logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,23 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  
+  
   def home
     redirect_to '/default/index'
   end
+  
+  before_filter :require_login
+
+private
+
+  def require_login
+    unless current_user || (request.path == login_path) || (request.path == signup_path)
+      puts request.path.inspect
+      redirect_to login_path, :flash => { :danger => "You must be logged in!" }
+    end
+  end
+end
+
+def current_user
+	@current_user ||= User.find_by(id: session[:user_id])
 end
