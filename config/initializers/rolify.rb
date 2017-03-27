8283172bcb2db857c003b58f9f2687bc39b4d5a9
Bradley Clark <bradley.j.clark@wmich.edu>
2017-03-27 19:13:36 -0400

    Remove comment

--- config/initializers/rolify.rb
+++ config/initializers/rolify.rb
@@ -4,5 +4,5 @@ Rolify.configure do |config|
 
   # Dynamic shortcuts for User class (user.is_admin? like methods). Default is: false
   # config.use_dynamic_shortcuts
-  #config.remove_role_if_empty=(false)
+  config.remove_role_if_empty=(false)
 end
