bf6dd4880fac6cb61bae5e22c84ac278d21b8a26
Joseph Owens <jtrumanowens@gmail.com>
2017-04-12 13:34:15 -0400

    shell tests deleted

--- spec/requests/customers_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe "Customers", type: :request do
-  describe "GET /customers" do
-
-  end
-end
--- spec/requests/palconfigs_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe "Palconfigs", type: :request do
-  describe "GET /palconfigs" do
-    
-  end
-end
